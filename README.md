2d114b4cdfd27bb9f8ea07c06c9775cb7d965bf6
Mike Hennelly <michaelthehennelly@gmail.com>
2021-03-23 18:14:03 -0700

    added README

---
+++ README.md
@@ -0,0 +1,8 @@
+# bashHistFreq
+
+Figure out which commands you should write aliases for
+
+---
+
+Prints a Pandas series ordered by value frequency, where the
+values are string sequences from .bash_history.
--- bash-hist.py
+++ bash-hist.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os, re
 
-pd.set_option('display.max_rows', None)
+pd.set_option('display.max_rows', 100) # Modify this how you'd like
 bashHistFile = open(os.environ['HOME'] + '/.bash_history')
-data = re.sub('[^A-Za-z0-9\-_@#$&*]', ' ', bashHistFile.read())
+data = re.sub('[^A-Za-z0-9\-_@#$&*`~.]', ' ', bashHistFile.read()) # And this
 bashHistFile.close()
 print(pd.Series(data.split()).value_counts())
 
